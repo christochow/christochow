165595459097a45277566b994dd91d829f86a85d
Chow Lok Man Christopher <christochow@yahoo.com.hk>
2020-03-09 23:43:20 -0400

    update to project overlay

--- src/pages/Projects/components/Overlay/OverlayComponent.js
+++ src/pages/Projects/components/Overlay/OverlayComponent.js
@@ -4,10 +4,16 @@ import './OverlayComponent.css'
 
 function Overlay(props) {
     return (
-        <div className="link-container">
-            <button><a href="http://google.com" target="_blank" rel="noopener noreferrer">see more</a></button>
-            <div className="space"></div>
-            <button><a href="http://google.com" target="_blank" rel="noopener noreferrer">see more</a></button>
+        <div>
+        {
+            props.detail.type === 'web' ? <div className="link-container">
+                <button className="btn-link"><a href={props.detail.link} target="_blank" rel="noopener noreferrer">Link to website</a></button>
+            </div> : <div className="link-container">
+                    <button className="btn-link"><a href={props.detail.ios} target="_blank" rel="noopener noreferrer">iOS</a></button>
+                    <div className="space"></div>
+                    <button className="btn-link"><a href={props.detail.android} target="_blank" rel="noopener noreferrer">Android</a></button>
+                </div>
+        }
         </div>
     );
 }
